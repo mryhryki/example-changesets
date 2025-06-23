5fe89c62183a3e221bd46de0c347724b7264dab9
mryhryki <mryhryki@gmail.com>
2025-06-23 16:10:36 +0900

    Update .github/scripts/create-release-drafter-yaml.mjs

--- .github/scripts/create-release-drafter-yaml.mjs
+++ .github/scripts/create-release-drafter-yaml.mjs
@@ -1,6 +1,38 @@
-import { writeFile } from 'fs/promises';
+import {
+  stat,
+  writeFile,
+} from 'fs/promises';
 
-const main = async ([dirName, version]) => {
+const getRequiredEnv = (name) => {
+  const value = process.env[name];
+  if (typeof value === 'string' && value.trim().length > 0) {
+    return value.trim();
+  }
+  throw new Error(`Environment variable ${name} is required`);
+};
+
+const main = async () => {
+  const refType = getRequiredEnv('GITHUB_REF_TYPE');
+  if (refType !== 'branch') {
+    throw new Error(`Expected GITHUB_REF_TYPE to be 'branch', got '${refType}'`);
+  }
+
+  const refName = getRequiredEnv('GITHUB_REF_NAME');
+  const names = refName.split('/');
+  if (names.length !== 3) {
+    throw new Error(`Expected GITHUB_REF_NAME to be in the format 'release/<dirName>/<version>', got '${refName}'`);
+  }
+
+  const [prefix, dirName, version] = names;
+  if (prefix !== 'release') {
+    throw new Error(`Expected GITHUB_REF_NAME to start with 'release/', got '${refName}'`);
+  }
+
+  const status = await stat(new URL(`../../units/${dirName}`, import.meta.url)).catch(() => null);
+  if (status?.isDirectory() !== true) {
+    throw new Error(`Directory 'units/${dirName}' does not exist`);
+  }
+
   const yaml = [
     'include-labels:',
     `- ${dirName}`,
@@ -26,7 +58,15 @@ const main = async ([dirName, version]) => {
   ].join('\n');
 
   const file = new URL('../release-drafter.yml', import.meta.url);
+  console.log(`Writing release drafter YAML to ${file.pathname}`);
+  console.log([
+    'YAML Content:',
+    '-----',
+    yaml,
+    '-----',
+  ].join('\n'));
+
   await writeFile(file, yaml, 'utf8');
 };
 
-main(process.argv.slice(2));
+main();
